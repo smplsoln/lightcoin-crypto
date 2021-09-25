22cd6bc93caa3f1f8ce95a96cd63a29728a861d8
Prashant R <smplsoln@gmail.com>
2021-09-25 02:23:49 -0600

    Add Ac and withdrawal and deposit tx

--- index.js
+++ index.js
@@ -1,29 +1,82 @@
-let balance = 500.00;
-
-class Withdrawal {
-
-  constructor(amount) {
+class Transaction {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
+  getValue() {
+    return this.amount;
+  }
   commit() {
-    balance -= this.amount;
+    if (!this.isAllowed()) {
+      return false;
+    }
+    this.account.addTransaction(this);
+    return true;
+  }
+
+  isAllowed() {
+    return true;
+  }
+}
+class Withdrawal extends Transaction {
+
+  getValue() {
+    return -(this.amount);
+  }
+
+  isAllowed() {
+    let remaining = this.account.balance - this.amount;
+    if (remaining >= 0) {
+      return true;
+    }
+    return false;
+  }
+}
+
+class Deposit extends Transaction {
+
+}
+
+class Account {
+  constructor(user) {
+    this.user = user;
+    this.transactions = [];
+  }
+
+  get balance() {
+    let val = 0;
+    for (const tx of this.transactions) {
+      val += tx.getValue();
+    }
+    return val;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
   }
 
 }
 
 
 
-
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const ac1 = new Account('user1');
+
+let t3 = new Deposit(1000, ac1);
+t3.commit();
+console.log({ t3 });
+console.log(`Account balance of ${ac1.user} is ${ac1.balance}`);
+
+let t1 = new Withdrawal(50.25, ac1);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+let t2 = new Withdrawal(9.99, ac1);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+console.log({ ac1 });
+console.log(`Account balance of ${ac1.user} is ${ac1.balance}`);
